3b9b535bb8cb821ce32bbf6f68d56bd9ed23a3ff
Keith Erdbruegger <kerdbruegger0@gmail.com>
2017-10-31 18:28:58 -0500

    idk

---
+++ GitFit/Models/ChangePasswordModel.swift
@@ -0,0 +1,70 @@
+//
+//  ChangePasswordModel.swift
+//  GitFit
+//
+//  Created by Ubicomp3 on 10/31/17.
+//  Copyright © 2017 Team3. All rights reserved.
+//
+
+import UIKit
+
+protocol ChangePasswordModelDelegate {
+    func passwordChanged(profile: Profile?)
+}
+
+class ChangePasswordModel: NSObject {
+    var delegate: ChangePasswordModelDelegate?
+
+    
+    let URL_UPDATE_PROFILE = "http://54.197.29.213/fitness/api/updateaccount.php"
+
+    func changePassword(id: Int, firstName: String, lastName: String, emailAddress: String, password: String) {
+        let requestUrl = URL(string: URL_UPDATE_PROFILE)
+        var request = URLRequest(url: requestUrl!)
+        
+        request.httpMethod = "PUT"
+        let postParameters = "id=" + String(id) + "&firstName=" + firstName + "&lastName=" + lastName + "&email=" + emailAddress + "&password=" + password
+        request.httpBody = postParameters.data(using: String.Encoding.utf8)
+        
+        URLSession.shared.dataTask(with: request) {
+            data, response, error in
+            guard let data = data, error == nil, response != nil else {
+                print("error in url session")
+                self.dispatchNoProfile()
+                return
+            }
+            
+            if let user = self.parseJson(data: data) {
+                DispatchQueue.main.async {
+                    self.delegate?.passwordChanged(profile: user)
+                }
+            }
+            else {
+                self.dispatchNoProfile()
+            }
+            }.resume()
+        
+    }
+    func dispatchNoProfile() {
+        DispatchQueue.main.async {
+            self.delegate?.passwordChanged(profile: nil)
+        }
+    }
+    
+    func parseJson(data: Data) -> Profile? {
+        do {
+            let decoder = JSONDecoder()
+            let users = try decoder.decode([Profile].self, from: data)
+            
+            if users.count > 0 {
+                return users[0]
+            }
+            else {
+                return nil
+            }
+        } catch {
+            print(error)
+            return nil
+        }
+    }
+}
---
+++ GitFit/Profile/ChangePasswordViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ChangePasswordViewController.swift
+//  GitFit
+//
+//  Created by Ubicomp3 on 10/31/17.
+//  Copyright © 2017 Team3. All rights reserved.
+//
+
+import UIKit
+
+class ChangePasswordViewController: UIViewController {
+   
+    @IBOutlet weak var currPass: UITextField!
+    @IBOutlet weak var newPass: UITextField!
+    @IBOutlet weak var verifyPass: UITextField!
+    @IBOutlet weak var submitButton: UIButton!
+    var id: Int?
+    var firstName: String?
+    var lastName: String?
+    var email: String?
+    var password: String?
+    let changePasswordModel = ChangePasswordModel()
+    
+    @IBAction func submit(_ sender: Any) {
+        loadValues()
+        changePasswordModel.changePassword(id: id!, firstName: firstName!, lastName: lastName!, emailAddress: email!, password: password!)
+    }
+    
+    func setButtonStyle() {
+        submitButton.layer.cornerRadius = submitButton.frame.height / 2
+        submitButton.clipsToBounds = true
+        submitButton.backgroundColor = fitBlue
+    }
+
+    func loadValues() {
+        id = user?.id ?? 0
+        firstName = user?.firstName ?? ""
+        lastName = user?.lastName ?? ""
+        email = user?.email ?? ""
+        password = newPass.text!
+//      user?.password = newPass.text!
+    }
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setButtonStyle()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

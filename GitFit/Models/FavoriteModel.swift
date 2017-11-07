b1c381d1bff132df0c84e724c2100616813183d1
Keith Erdbruegger <kerdbruegger0@gmail.com>
2017-11-06 22:28:11 -0600

    font type

--- GitFit/Models/FavoriteModel.swift
+++ GitFit/Models/FavoriteModel.swift
@@ -8,6 +8,8 @@ class FavoriteModel: NSObject {
     var delegate: FavoriteModelDelegate?
     
     let URL_FAVORITES = "http://54.197.29.213/fitness/api/getfavorites.php"
+    let URL_ADD_FAVORITE = "http://54.197.29.213/fitness/api/addfavorite.php"
+    let URL_REMOVE_FAVORITE = "http://54.197.29.213/fitness/api/removefavorite.php"
     
     func loadWorkouts(_ accountId: Int) {
         let params = "accountId=\(accountId)"
@@ -43,6 +45,44 @@ class FavoriteModel: NSObject {
             print(error)
             return []
         }
+    }
+    
+    func addFavorite(_ accountId: Int, _ workoutId: Int) {
+        let requestUrl = URL(string: URL_ADD_FAVORITE)
+        var request = URLRequest(url: requestUrl!)
+        
+        request.httpMethod = "POST"
+        let postParameters = "accountId=\(accountId)&workoutId=\(workoutId)"
+        request.httpBody = postParameters.data(using: String.Encoding.utf8)
+        
+        URLSession.shared.dataTask(with: request) {
+            data, response, error in
+            guard let data = data, error == nil, response != nil else {
+                print("error in url session")
+                return
+            }
+            
+            print("Favorited: \(String(data: data, encoding: .utf8) ?? "")")
+        }.resume()
+    }
+    
+    func removeFavorite(_ accountId: Int, _ workoutId: Int) {
+        let requestUrl = URL(string: URL_REMOVE_FAVORITE)
+        var request = URLRequest(url: requestUrl!)
+        
+        request.httpMethod = "DELETE"
+        let postParameters = "accountId=\(accountId)&workoutId=\(workoutId)"
+        request.httpBody = postParameters.data(using: String.Encoding.utf8)
+        
+        URLSession.shared.dataTask(with: request) {
+            data, response, error in
+            guard let data = data, error == nil, response != nil else {
+                print("error in url session")
+                return
+            }
+            
+            print("Removed: \(String(data: data, encoding: .utf8) ?? "")")
+        }.resume()
     }
 }
 

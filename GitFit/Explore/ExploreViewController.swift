dcf5c52a9e9856f04c721457350d85b531f3b247
sumaiyaasif <sumaiya.aa@gmail.com>
2017-12-06 14:45:50 -0600

    two things on explore page. exercises added

--- GitFit/Explore/ExploreViewController.swift
+++ GitFit/Explore/ExploreViewController.swift
@@ -8,26 +8,67 @@
 
 import UIKit
 
-class ExploreViewController: UIViewController {
+class ExploreViewController: UIViewController, CreateModelDelegate {
     @IBOutlet weak var featuredExercise: UIImageView!
+    @IBOutlet weak var featuredUser: UIImageView!
+    let createModel = CreateModel()
+    var exercises: [Exercise] = []
     
+    func exercisesLoaded(exercises: [Exercise]) {
+        self.exercises = exercises
+    }
+    
+    func getRandomExercise() -> Exercise? {
+        if exercises.count <= 0 { return nil }
+        let rand = Int(arc4random_uniform(UInt32(exercises.count)))
+        return exercises[rand]
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap(sender:)))
-        featuredExercise.addGestureRecognizer(tap)
-        
+        createModel.loadExercises(search: "")
+        createModel.delegate = self
+        let tapToExercise = UITapGestureRecognizer(target: self, action: #selector(handleTapToExercise(sender:)))
+        let tapToUser = UITapGestureRecognizer(target: self, action: #selector(handleTapToUser(sender:)))
+        featuredExercise.addGestureRecognizer(tapToExercise)
+        featuredUser.addGestureRecognizer(tapToUser)
+        featuredUser.isUserInteractionEnabled = true
+        featuredExercise.isUserInteractionEnabled = true
         // Do any additional setup after loading the view.
     }
     
-    @objc func handleTap(sender: UITapGestureRecognizer) {
+    @objc func handleTapToExercise(sender: UITapGestureRecognizer) {
         if sender.state == .ended {
-            UIView.animate(withDuration: 0.5, delay: 0.5, options: .curveEaseOut, animations: {
-                self.featuredExercise.alpha = 0.0
-            }, completion: nil)
+//            UIView.animate(withDuration: 0.5, delay: 0.5, options: .curveEaseOut, animations: {
+//                self.featuredExercise.alpha = 0.0
+//            }, completion: nil)
             // handling code
+            performSegue(withIdentifier: "exploreToExercises", sender: self)
         }
     }
+    
+    @objc func handleTapToUser(sender: UITapGestureRecognizer) {
+        if sender.state == .ended {
+            performSegue(withIdentifier: "exploreToUsers", sender: self)
+        }
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "exploreToUsers"{
+            let dest = segue.destination as! ExerciseProfileViewController
+            dest.creatorId = 1
+            dest.profilePic = #imageLiteral(resourceName: "profile_pic_placeholder")
+        }
+        else if segue.identifier == "exploreToExercises"{
+            let dest = segue.destination as! ExerciseViewController
+            if let exercise = getRandomExercise() {
+                dest.descripe = exercise.exerciseDescription
+                dest.id = exercise.id
+                dest.name = exercise.name
+                dest.information = ""
+            }
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
